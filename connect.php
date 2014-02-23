60090a13fc45e87d71f0fbd8cba6ab337a7bdff9
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-24 00:28:44 +0530

    second commit

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost:3306";
-$pass="ridwan";
-$user="tharindu";
-$db="rt_games";
+$host="ec2-184-72-238-68.compute-1.amazonaws.com";
+$pass="BkkLQbWiS5jzwHfwkxFGDnot17";
+$user="erifhknmlngjqb";
+$db="d6ferf43cg9e2h";
 $link=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
 
 $selcet=mysql_select_db($db,$link) or die("couldn't find the database:". mysql_error());
