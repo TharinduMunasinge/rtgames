4e30137c9a078f5775acd6b2e66821669f73d266
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-24 00:55:38 +0530

    third commit

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host="ec2-184-72-238-68.compute-1.amazonaws.com";
-$pass="BkkLQbWiS5jzwHfwkxFGDnot17";
-$user="erifhknmlngjqb";
-$db="d6ferf43cg9e2h";
+$host="mysql.pixelexplorere.com";
+$pass="tharpixel";
+$user="pixeltharindu";
+$db="pixelwordpress";
 $link=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
 
 $selcet=mysql_select_db($db,$link) or die("couldn't find the database:". mysql_error());
