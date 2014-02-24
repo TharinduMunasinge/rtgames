b72cfe453aa4b12cee5fa75cc043c9632f303a76
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-24 22:07:43 +0530

    8th comit

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $host="mysql.pixelzexplorer.org";
 
-$pass="tharpixel";
-$user="'pixeltharindu";
-$db="pixelwordpress";
+$pass="pixelz313";
+$user="'adminpixelz";
+$db="wikilanka";
 $link=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
 
 $selcet=mysql_select_db($db,$link) or die("couldn't find the database:". mysql_error());
