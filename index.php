9426cad6b1fce25f1b529b2a3ae329e096c1b0e7
ridwannaruto <ridwan.11@cse.mrt.ac.lk>
2014-02-25 00:31:55 +0530

    iehgi

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ echo "hello";
 $user="adminpixelz";
 $pass="pixelz313";
 $db="wikilanka";
+$host="mysql.pixelzexplorer.org";
 
+
+$dbhandle=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
 
 
 
