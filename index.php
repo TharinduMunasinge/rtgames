0ed11f3e92f1a08756c29ae6cc67e7411079366c
ridwannaruto <ridwan.11@cse.mrt.ac.lk>
2014-02-25 00:29:30 +0530

    iehgi

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
-eufhduigh
+
 <?php
 
 
 echo "hello";
 echo "hello";
-$user="adminpixelz";
-$pass="pixelz313";
-$db="wikilanka";
-=======
-//$pass=$url['pass'];
-//$user=$url['user'];
-//$db=substr($url['path'],1);
 
-//$dbhandle=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
-
-//$selcet=mysql_select_db($db,$link) or die("couldn't find the database:". mysql_error());
-
-//$result=mysql_query("INSERT INTO `wikilanka`.`subscriber` SET `name`='Ridwan'",$dbhandle);
-echo ($result);
-echo "PL";
 
 ?>
 
