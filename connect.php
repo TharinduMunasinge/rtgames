fc878af3512eea225efcc45a44f1d3dafc5d8b48
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-24 23:02:34 +0530

    11th commit

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
-$host="mysql.pixelzexplorer.org";
+$url=parse_url(getenv("DATABASE_URL"));
+$host=$url['host'];
 
-$pass="pixelz313";
-$user="'adminpixelz";
-$db="wikilanka";
+$pass=$url['pass'];
+$user=$url['user'];
+$db=substr($url['path'],1);
 $link=mysql_connect($host,$user,$pass) or die("couldn't connect the host ".mysql_error());
 
 $selcet=mysql_select_db($db,$link) or die("couldn't find the database:". mysql_error());
