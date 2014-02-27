45d928a5f34f45a8b78b3f12bef18231bffeaa30
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-27 22:42:08 +0530

    dd comment

--- testmail.php
+++ testmail.php
@@ -1,7 +1,7 @@
 <?php
-require ("Postmark/Mail.php");
+require_once dirname(__FILE__).'/Postmark/Autoloader.php';
 $postmarkApiKey = '518ceed1-dfe1-43b9-b103-ed5320ea2953';
-
+echo "test";
 // Create a message and send it
 PostMark\Mail::compose($postmarkApiKey)
     ->from('rtgames@heroku.com')
