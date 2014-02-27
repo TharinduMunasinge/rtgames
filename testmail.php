32602dcb2f0f325857a8b4070f8c606bacf61367
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-27 23:12:50 +0530

    dd comment

--- testmail.php
+++ testmail.php
@@ -5,7 +5,7 @@ $postmarkApiKey = '518ceed1-dfe1-43b9-b103-ed5320ea2953';
 echo "test";
 // Create a message and send it
 Postmark\Mail::compose($postmarkApiKey)
-    ->from('rtgames@heroku.com')
+    ->from('b35d3bf37b034f3fe285ad8d7397fca7@inbound.postmarkapp.com')
     ->addTo('munasinghetharindu@gmail.com')
     ->subject('Subject')
     ->messagePlain('Plaintext message')
