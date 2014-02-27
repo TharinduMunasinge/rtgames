b7e0c06559e74d6ef6e5b3aebe5792f2298e032e
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-27 21:52:27 +0530

    dd comment

--- testmail.php
+++ testmail.php
@@ -1,3 +1,14 @@
 <?php
-mail('munasinghetharindu@gmail.com', "Subject:test","hello", "From:no-reply@rtgames.com" );
+$postmarkApiKey = '518ceed1-dfe1-43b9-b103-ed5320ea2953';
+
+// Create a message and send it
+Postmark\Mail::compose($postmarkApiKey)
+    ->from('rtgames@heroku.com', 'John Smith')
+    ->addTo('munasinghetharindu@gmail.com', 'Jane Smith')
+    ->subject('Subject')
+    ->messagePlain('Plaintext message')
+    ->send();
+
+
+//mail('munasinghetharindu@gmail.com', "Subject:test","hello", "From:no-reply@rtgames.com" );
 ?>
