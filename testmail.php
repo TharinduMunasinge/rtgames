08c146e89129b0cbcf8230c3f121b539a829ca56
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2014-02-27 21:37:20 +0530

    dd comment

--- testmail.php
+++ testmail.php
@@ -1,3 +1,3 @@
 <?php
-mail('munasinghetharindu@gmail.com', "Subject: $subject",$message, "From:no-reply@rtgames.com" );
+mail('munasinghetharindu@gmail.com', "Subject:test","hello", "From:no-reply@rtgames.com" );
 ?>
